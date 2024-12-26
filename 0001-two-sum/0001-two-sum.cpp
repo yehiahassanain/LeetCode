32caa7ed49db69e075b14fc3ec5b7ab67cad29ca
yehiahassanain <89047698+yehiahassanain@users.noreply.github.com>
2024-12-26 23:59:18 +0200

    Time: 3 ms (77.16%), Space: 15.2 MB (11.53%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-map<int,int>mp;
+        map<int,int>mp;
         vector<int>res;
-        for (int i=0;i<nums.size();i++){
-            mp.insert({nums[i],i});
-        }
         for (int i=0;i<nums.size();i++){
             int val = target - nums[i];
             auto sec_num = *mp.find(val);
@@ -14,6 +11,7 @@ map<int,int>mp;
                 res.push_back(sec_num.second);
                 break;
             }
+            mp.insert({nums[i],i});
         }
         return res;
     }
