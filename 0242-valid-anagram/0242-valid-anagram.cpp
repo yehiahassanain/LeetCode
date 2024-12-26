a32b82a7a333b698ed7bb1db89d311b2c30016f0
yehiahassanain <89047698+yehiahassanain@users.noreply.github.com>
2024-12-27 00:55:18 +0200

    Time: 83 ms (6.44%), Space: 9.6 MB (57.17%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.size()!=t.size())
-            return false;
+        if (s.size()!=t.size()) return false;
         for ( int i = 0 ; i < s.size() ; i++) {
             if (s.find(t[i]) == std::string::npos) {
                 return false;
             }
             if (s.find(t[i]) != std::string::npos) {
-                // cout<<s.find(t[i])<<endl;
                 s[s.find(t[i])] = '0';
             }
         }
